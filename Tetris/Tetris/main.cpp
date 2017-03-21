bf30f104acd484e9b54f0797db228961dfb7cdae
ArunoxPL <greg.malek@gmail.com>
2017-03-21 12:13:54 +0100

    Making a Frooger game

--- Tetris/Tetris/main.cpp
+++ Tetris/Tetris/main.cpp
@@ -1,157 +1,72 @@
 #include <SFML/Graphics.hpp>
-#include <time.h>
-using namespace sf;
-
-const int M = 20;
-const int N = 10;
-
-// Postaram sie jeszce to dodac
-
-int field[M][N] = { 0 };
-
-struct Point
-{
-	int x, y;
-} a[4], b[4];
-
-int figures[7][4] =
-{
-	1, 3, 5, 7, // I
-	2, 4, 5, 7, // Z
-	3, 5, 4, 6, // S
-	3, 5, 4, 7, // T
-	2, 3, 5, 7, // L
-	3, 5, 7, 6, // J
-	2, 3, 4, 5, // O
-};
-
-bool check()
-{
-	for (int i = 0; i<4; i++)
-		if (a[i].x<0 || a[i].x >= N || a[i].y >= M) return 0;
-		else if (field[a[i].y][a[i].x]) return 0;
-
-		return 1;
-};
-
 
 int main()
 {
-	srand(time(0));
+	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Starter Template");
 
-	RenderWindow window(VideoMode(320, 480), "The Game!");
+	sf::RectangleShape frog;
 
-	Texture t1, t2, t3;
-	t1.loadFromFile("data/images/tiles.png");
-	t2.loadFromFile("data/images/background.png");
-	t3.loadFromFile("data/images/frame.png");
+	frog.setSize(sf::Vector2f(80, 80));
+	frog.setPosition(sf::Vector2f(frog.getPosition().x, window.getSize().y - frog.getSize().y));
 
-	Sprite s(t1), background(t2), frame(t3);
 
-	int dx = 0; bool rotate = 0; int colorNum = 1;
-	float timer = 0, delay = 0.3;
+	sf::RectangleShape truck;
 
-	Clock clock;
+	truck.setSize(sf::Vector2f(120, 60));
+	truck.setFillColor(sf::Color::Red);
+	truck.setPosition(sf::Vector2f(truck.getPosition().x, window.getSize().y / 2 - truck.getSize().y));
 
 	while (window.isOpen())
 	{
-		float time = clock.getElapsedTime().asSeconds();
-		clock.restart();
-		timer += time;
+		// handle events
+		sf::Event event;
 
-		Event e;
-		while (window.pollEvent(e))
+		while (window.pollEvent(event))
 		{
-			if (e.type == Event::Closed)
-				window.close();
-
-			if (e.type == Event::KeyPressed)
-				if (e.key.code == Keyboard::Up) rotate = true;
-				else if (e.key.code == Keyboard::Left) dx = -1;
-				else if (e.key.code == Keyboard::Right) dx = 1;
-		}
-
-		if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
-
-		//// <- Move -> ///
-		for (int i = 0; i<4; i++)  { b[i] = a[i]; a[i].x += dx; }
-		if (!check()) for (int i = 0; i<4; i++) a[i] = b[i];
-
-		//////Rotate//////
-		if (rotate)
-		{
-			Point p = a[1]; //center of rotation
-			for (int i = 0; i<4; i++)
+			switch (event.type)
 			{
-				int x = a[i].y - p.y;
-				int y = a[i].x - p.x;
-				a[i].x = p.x - x;
-				a[i].y = p.y + y;
-			}
-			if (!check()) for (int i = 0; i<4; i++) a[i] = b[i];
-		}
+			case sf::Event::KeyReleased:
 
-		///////Tick//////
-		if (timer>delay)
-		{
-			for (int i = 0; i<4; i++) { b[i] = a[i]; a[i].y += 1; }
-
-			if (!check())
-			{
-				for (int i = 0; i<4; i++) field[b[i].y][b[i].x] = colorNum;
-
-				colorNum = 1 + rand() % 7;
-				int n = rand() % 7;
-				for (int i = 0; i<4; i++)
+				if (sf::Keyboard::Key::Left == event.key.code)
 				{
-					a[i].x = figures[n][i] % 2;
-					a[i].y = figures[n][i] / 2;
+					frog.move(-80, 0);
+				}
+				else if (sf::Keyboard::Key::Right == event.key.code)
+				{
+					frog.move(80, 0);
+				}
+				else if (sf::Keyboard::Key::Up == event.key.code)
+				{
+					frog.move(0, -80);
+				}
+				else if (sf::Keyboard::Key::Down == event.key.code)
+				{
+					frog.move(0, 80);
 				}
 			}
-
-			timer = 0;
 		}
 
-		///////check lines//////////
-		int k = M - 1;
-		for (int i = M - 1; i>0; i--)
+		// update
+		truck.move(0.2, 0);
+
+		if (truck.getPosition().x > window.getSize().x)
 		{
-			int count = 0;
-			for (int j = 0; j<N; j++)
-			{
-				if (field[i][j]) count++;
-				field[k][j] = field[i][j];
-			}
-			if (count<N) k--;
+			truck.setPosition(sf::Vector2f(-truck.getSize().x, window.getSize().y / 2 - truck.getSize().y));
 		}
 
-		dx = 0; rotate = 0; delay = 0.3;
-
-		/////////draw//////////
-		window.clear(Color::White);
-		window.draw(background);
-
-		for (int i = 0; i<M; i++)
-			for (int j = 0; j<N; j++)
-			{
-				if (field[i][j] == 0) continue;
-				s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
-				s.setPosition(j * 18, i * 18);
-				s.move(28, 31); //offset
-				window.draw(s);
-			}
-
-		for (int i = 0; i<4; i++)
+		if (frog.getGlobalBounds().intersects(truck.getGlobalBounds()))
 		{
-			s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-			s.setPosition(a[i].x * 18, a[i].y * 18);
-			s.move(28, 31); //offset
-			window.draw(s);
+			window.close();
 		}
 
-		window.draw(frame);
+		window.clear();
+
+		// draw SFML content
+		window.draw(frog);
+		window.draw(truck);
+
 		window.display();
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
